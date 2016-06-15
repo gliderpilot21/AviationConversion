4dec1b9271bee2854f40dde631d5d886f444cfc1
gliderpilot21 <protog.60@gmail.com>
2016-06-15 20:38:24 +0200

    added functions to convert km/h / kn and ft / m

---
+++ avConversion.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+def kmhToKn(kmh):
+    '''Umrechnung km/h zu kn'''
+    return kmh / 1.852
+
+def knToKmh(kn):
+    '''Umrechnung kn zu km/h'''
+    return kn * 1.852
+
+def mToFt(m):
+    '''Umrechnung m zu ft'''
+    return m / 0.3048
+
+def ftToM(ft):
+    '''Umrechnung ft zu m'''
+    return ft * 0.3048
