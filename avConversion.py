30bdcca1c3a0e5c8f561def51165f9fbc2878f39
gliderpilot21 <protog.60@gmail.com>
2016-06-17 18:55:05 +0200

    separated conversion functions from the rest of the code. --> conversions are easier to make in other projects when using the new separated file

--- avConversion.py
+++ avConversion.py
@@ -1,45 +1,5 @@
 # -*- coding: utf-8 -*-
-
-def kmhToKn(kmh):
-    '''Umrechnung km/h zu kn'''
-    return kmh / 1.852
-
-def knToKmh(kn):
-    '''Umrechnung kn zu km/h'''
-    return kn * 1.852
-
-def mToFt(m):
-    '''Umrechnung m zu ft'''
-    return m / 0.3048
-
-def ftToM(ft):
-    '''Umrechnung ft zu m'''
-    return ft * 0.3048
-
-def kgToLb(kg):
-    '''Umrechnung kg zu lb'''
-    return kg * 2.20462262185
-
-def lbToKg(lb):
-    '''Umrechnung lb zu kg'''
-    return lb / 2.20462262185
-
-def mpsToFtps(mps):
-    '''Umrechnung m/s zu ft/min'''
-    return mps * 0.00508
-
-def ftpsToMps(ftps):
-    '''Umrechnung ft/min zu m/s'''
-    return ftps / 0.00508
-
-def hpaToInhg(hpa):
-    '''Umrechnung hPa zu in Hg'''
-    return hpa / 33.8638866667
-
-def inhgToHpa(inhg):
-    '''Umrechnung inHg zu hPa'''
-    return hPa * 33.8638866667
-
+from conversions import *
 
 def selection():
     print("Welcome to the aviation unit converter!")
